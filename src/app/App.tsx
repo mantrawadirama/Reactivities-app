f70039c6f94dcdbb9f2568c87622d9ddff1c2d2c
mantrawadirama <mantrawadirama@gmail.com>
2020-05-12 11:35:32 -0500

    folder structure and typescript addition

---
+++ src/app/App.tsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import { Header, Icon, List } from 'semantic-ui-react'
+
+import axios from 'axios'
+import { IActivity } from '../models/activity'
+
+interface IState {
+  activities: IActivity[]
+}
+export class App extends Component<{}, IState> {
+  state: IState = {
+    activities: [],
+  }
+  componentDidMount() {
+    axios
+      .get<IActivity[]>('http://localhost:5000/api/activities')
+      .then((response) => {
+        this.setState({
+          activities: response.data,
+        })
+      })
+  }
+  render() {
+    return (
+      <div>
+        <Header as='h2'>
+          <Icon name='users' />
+          <Header.Content>Reactivities</Header.Content>
+        </Header>
+        <List>
+          {this.state.activities.map((activity) => (
+            <List.Item key={activity.id}>{activity.title}</List.Item>
+          ))}
+        </List>
+      </div>
+    )
+  }
+}
+export default App
